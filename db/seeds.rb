8afdd6ee0ffeba7c766bae56088d961ecd778a7b
Mike Shirokowsky <shirokowsky@gmail.com>
2015-11-01 17:14:31 -0500

    password_confirm patch

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 users = User.create([
   { nickname: 'Admin',
     email: 'admin@example.com',
     password: '123',
-    password_confirmation: '12345678',
+    password_confirmation: '123',
     admin: true
   },
   { nickname: 'User',
     email: 'user@example.com',
     password: '123',
-    password_confirmation: '12345678',
+    password_confirmation: '123',
   }
 ])
 
